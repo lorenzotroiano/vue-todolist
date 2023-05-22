182f30732a3661779964c5644496eec2c0a2cf24
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-22 15:24:35 +0200

    perfezionato css e html

---
+++ js/list.js
@@ -0,0 +1,19 @@
+/*
+
+
+oggetto, formato da due proprietà:
+- text, una stringa che indica il testo del task
+- done, un booleano (true/false) che indica se il task è stato fatto oppure no
+MILESTONE 1
+Stampare all’interno di una lista HTML un item per ogni task.
+Se la proprietà done è uguale a true, visualizzare il testo del task sbarrato.
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il task viene rimosso dalla lista.
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo task, che quindi viene aggiunto alla lista dei task esistenti.
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il task alla lista
+2- cliccando sul testo dell’item, invertire il valore della proprietà done del task corrispondente (se done era uguale a false, impostare true e viceversa)
+
+
+*/
